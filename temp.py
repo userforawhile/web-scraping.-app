6eb7effeddd0840ed6724fbfaaf75dfc253dee66
Antonio Marino <marino.ant@yahoo.com>
2023-03-16 14:09:49 +0100

    Add sqlite3 to store the data extracted

--- temp.py
+++ temp.py
@@ -1,12 +1,13 @@
 import requests
 import selectorlib
 from datetime import datetime
+import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-
+connection = sqlite3.connect("temp.db")
 def scrape(url):
     """scrape the source page form the url"""
     response = requests.get(url, headers=HEADERS)
@@ -22,9 +23,11 @@ def extract(source):
 
 def store(extracted):
     now = datetime.now().strftime("%y-%m-%d-%H %M %S")
-    with open("data1.txt", "a") as file:
-        line = f"{now}, {extracted}\n"
-        file.write(line)
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperatura VALUES(?,?)", (now, extracted))
+    connection.commit()
 
 
 if __name__ == "__main__":
--- webapp.py
+++ webapp.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import plotly.express as px
-import pandas
+import sqlite3
 
-df = pandas.read_csv("data1.txt", on_bad_lines='skip')
+connection = sqlite3.connect("temp.db")
+cursor = connection.cursor()
+cursor.execute("SELECT date from temperatura")
+date = cursor.fetchall()
+date = [item[0] for item in date]
 
-figure = px.line(x=df["date"], y=df["temperature"],
+cursor.execute("SELECT temperature from temperatura")
+temperature = cursor.fetchall()
+temperature = [item[0] for item in temperature]
+
+
+figure = px.line(x=date, y=temperature,
                  labels={"x": "Date", "y": "Temperature (C)"})
 
 st.plotly_chart(figure)
